ad99e770475087a532f09f4303c305244363d936
Eugene <eugene.tikhon@gmail.com>
2024-01-05 15:43:35 +0100

    added Login Page and Auth Functionality

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,13 +2,13 @@ import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <section className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <aside className="w-full flex-none md:w-64">
         <SideNav />
       </aside>
       <section className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        <article>{children}</article>
+        {children}
       </section>
-    </main>
+    </section>
   );
 }
